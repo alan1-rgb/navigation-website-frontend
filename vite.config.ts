2dd0ab10e465de48a2bb144c397c0716ccd4899e
kevin <drwangly2017@gmail.com>
2025-12-21 02:37:02 +0800

    改为huggingface后端api

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        // target: 'http://localhost:3001',
-        target: 'https://alan6667-backend-navigation-website.hf.space',
+        target: 'http://localhost:3001',
+        // target: 'https://alan6667-backend-navigation-website.hf.space',
         changeOrigin: true,
         secure: false,
       }
